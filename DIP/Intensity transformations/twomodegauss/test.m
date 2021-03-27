e97c7131905167bce7279c6a3ab97cb6f458c579
zoxiii <zoxiii@163.com>
2021-03-27 15:12:24 +0800

    zoxiii-master-修改默认分支内容

--- DIP/Intensity transformations/twomodegauss/test.m
+++
@@ -1,6 +0,0 @@
-p = twomodegauss(0.15,0.05, 0.75, 0.05,1, 0.07, 0.002);
-RGB=imread('D:\Project_All_Apps\DIP_project\Images\OTHER\kris(1).jpg');%¶ÁÈ¡RGBÍ¼Ïñ
-I = rgb2gray(RGB); %Í¼Ïñ×ª»»Îª»ÒÉ«Í¼Ïñ
-g = histeq(I, p);  %
-figure, imshow(g)
-figure, imhist(g)
